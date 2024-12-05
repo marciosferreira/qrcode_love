1f82a7e7e637414bdf811bfd8b5a3fbe83f8409e
Marcio Soares Ferreira <marcio.ferreira@venturus.org.br>
2024-12-04 21:07:54 -0400

    dynamo credentialsz

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ timezone = pytz.timezone('America/Manaus')
 app = Flask(__name__)
 
 region_name='us-east-1',
-aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
+aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID')
 aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY')
 print(aws_secret_access_key)
 print(aws_access_key_id)
